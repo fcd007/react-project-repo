dfe63ec7c487b725071829fa825a53b9f0c7b511
Claudeilton Dantas <fcd007@hotmail.com>
2020-09-27 09:33:34 -0300

    update styles dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import logoImg from '../../assets/logo.svg';
 
-import { Title } from './styles';
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore Repository no Github </Title>
+    return (
+        <>
+            <img src={ logoImg } alt="Github Explorer" />
+            <Title>Explore Repository no Github </Title>
+
+            <Form >
+                <input type="text" placeholder="Digite o nome do repostório"/>
+            </Form>
+        </>
+    );
 }
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -4,4 +4,23 @@ import styled from 'styled-components';
 export const Title = styled.h1`
     font-size: 40px;
     color: #3a3a3a;
+    max-width: 450px;
+    margin-top: 80px;
+    line-height: 56px;
+`;
+
+//template literals
+export const Form = styled.form`
+    margin-top: 40px;
+    max-width: 700px;
+
+    display: flex;
+
+    input {
+        flex: 1;
+    }
+
+    button {
+        width: 210px;
+    }
 `;
