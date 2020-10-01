798a4411554d7b10ce9df26c2162125e271fa50a
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-01 05:58:59 -0300

    add route for detals repositories and styles

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
     repository:  string;
@@ -8,7 +12,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params }= useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository: { params.repository }</h1>
-}
+    return (
+        <>
+            <Header>
+                <img src={ logoImg } alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={ 16 }/>
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars3.githubusercontent.com/u/23359046?s=460&u=90066c4268b5c0489d541dafa129f2eab08407cb&v=4" 
+                    alt="Facebook/react"/>
+                    <div>
+                        <strong>facebook/react</strong>
+                        <p>Decrição do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                    <Link to= {"Teste to"} >
+                        <div>
+                            <strong>TItle</strong>
+                            <p>descrition</p>
+                        </div>
+                        <FiChevronRight size={ 20 }/>
+                    </Link>
+            </Issues>
+        </>
+    );
+};
 
 export default Repository;
