b6e24c12464250432e7f64df739765bb3eb712cc
Claudeilton Dantas <fcd007@hotmail.com>
2020-09-30 22:00:07 -0300

    add routes correct repositories/name/repo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState, FormEvent, useEffect } from 'react';
-import logoImg from '../../assets/logo.svg';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api';
+import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories, Error } from './styles';
 
@@ -74,7 +76,10 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 { repositories.map(repository => (
-                    <a key={ repository.full_name } href="http://">
+                    <Link 
+                        key={ repository.full_name } 
+                        to= {`/repositories/${repository.full_name }`}
+                    >
                     <img 
                         src={ repository.owner.avatar_url } 
                         alt= { repository.owner.login }
@@ -84,7 +89,7 @@ const Dashboard: React.FC = () => {
                     <p>{ repository.description }</p>
                     </div>
                     <FiChevronRight size={ 20 }/>
-                </a>
+                </Link>
                 ))}
             </Repositories>
         </>
